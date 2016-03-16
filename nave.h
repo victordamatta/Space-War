1c92394b65c2e70da8ab89ea8c5d6ebc5478e997
victormatta <victormatta14@gmail.com>
2016-03-16 19:27:35 -0300

    write nave interface

---
+++ nave.h
@@ -0,0 +1,23 @@
+struct Nave {
+    char* nome,
+    double massa,
+    double x,
+    double y,
+    double velx,
+    double vely
+};
+
+typedef struct Nave * nave;
+
+/* Constructor para uma nave */
+nave nova_nave (char* nome, double massa, double x, double y, double velx, double vely);
+
+/* Altera a velocidade da nave n de acordo com o vetor forca f
+ * aplicado com duracao dt */
+void empurra_nave (nave n, forca f, double dt);
+
+/* Calcula a atracao gravitacional da nave n no ponto (x, y) */
+double atracao_nave (nave n, double x, double y);
+
+/* Altera as coordenadas da nave de acordo com a velocidade no tempo dt */
+void atualiza_nave (nave n, double dt);
